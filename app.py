e45776e2c67377f19bac6676ca9162725073ccbb
danonbrez <153354844+danonbrez@users.noreply.github.com>
2024-06-30 23:40:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # This will enable CORS for all routes
 
 @app.route('/', methods=['GET'])
 def home():
